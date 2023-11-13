d6c59dac0775b65e394ff4c47dd18afc1358023e
Benson <benson.yeh.phone@gmail.com>
2023-11-13 23:31:27 +0800

    Initial Commit

---
+++ funny_app/funny_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  funny_appApp.swift
+//  funny_app
+//
+//  Created by 李青芬 on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct funny_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
